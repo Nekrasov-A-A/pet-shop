5cae1bda75e78d77cb9729143b60492a9c7373f0
nekrasov-a-a <a.alex.nekrasov@gmail.com>
2022-08-12 18:28:53 +0300

    feat(firebase): add on_auth_state_change to control user status

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,28 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
+
+<script>
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { mapActions } from "vuex";
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    this.controlAuth();
+  },
+  methods: {
+    ...mapActions(["changeLoggedIn"]),
+    controlAuth() {
+      const auth = getAuth();
+      this.changeLoggedIn(auth);
+      onAuthStateChanged(auth, () => {
+        this.changeLoggedIn(!!auth.currentUser);
+      });
+    },
+  },
+};
+</script>
