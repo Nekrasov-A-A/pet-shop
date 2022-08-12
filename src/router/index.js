94acd6e544dfce0c38437b27b22f8a9b9a244750
nekrasov-a-a <a.alex.nekrasov@gmail.com>
2022-08-12 15:21:22 +0300

    feat(vue-router): add routes
    protect route /personal-account by AuthMiddleware

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,60 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AuthMiddleWare from "./middleware/auth-middleware.js";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-  },
-];
-
-const router = new VueRouter({
+export default new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("../views/RegisterView.vue"),
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("../views/LoginView.vue"),
+    },
+    {
+      path: "/personal-account",
+      name: "personal-account",
+      meta: { middleware: AuthMiddleWare },
+      component: () => import("../views/PersonalAccountView.vue"),
+    },
+    {
+      path: "/order",
+      name: "order",
+      component: () => import("../views/OrderView.vue"),
+    },
+    {
+      path: "/catalog",
+      name: "catalog",
+      component: () => import("../views/CatalogView.vue"),
+    },
+    {
+      path: "/catalog/:category",
+      name: "category",
+      props: true,
+      component: () => import("../views/CategoryView.vue"),
+    },
+    {
+      path: "/lookbook",
+      name: "lookbook",
+      component: () => import("../views/LookbookView.vue"),
+    },
+    {
+      path: "*",
+      name: "not-found",
+      component: () => import("../views/NotFoundView.vue"),
+    },
+  ],
 });
-
-export default router;
