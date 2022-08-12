c441564c863ff106c14298afc6bf6a9bb19c2676
nekrasov-a-a <a.alex.nekrasov@gmail.com>
2022-08-12 11:00:41 +0300

    feat(plugins): init firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+
+initializeApp({
+  apiKey: "AIzaSyCpdacJNwh7qxUKeKdp5DT3vTSDwoijvv4",
+  authDomain: "pet-shop-b3e22.firebaseapp.com",
+  projectId: "pet-shop-b3e22",
+  storageBucket: "pet-shop-b3e22.appspot.com",
+  messagingSenderId: "744286127439",
+  appId: "1:744286127439:web:4ab4971d7351439591713a",
+});
