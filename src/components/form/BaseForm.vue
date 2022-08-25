642893ff17503a6989ddbfb3859f55e7e1b1796c
nekrasov-a-a <a.alex.nekrasov@gmail.com>
2022-08-25 20:30:08 +0300

    feat(components): add BaseForm.vue

---
+++ src/components/form/BaseForm.vue
@@ -0,0 +1,70 @@
+<template>
+  <ValidationObserver v-slot="{ handleSubmit }">
+    <form
+      :class="$style.form"
+      @submit.prevent="handleSubmit(onSubmit)"
+      novalidate
+    >
+      <h3 :class="$style.form_headline">{{ headline }}</h3>
+      <EmailInput />
+      <PasswordInput placeholderInput="Введите пароль" />
+      <button :class="$style.form_submit">
+        <slot>submit</slot>
+      </button>
+    </form>
+  </ValidationObserver>
+</template>
+
+<script>
+import { ValidationObserver } from "vee-validate";
+import "./rules";
+import PasswordInput from "./inputs/PasswordInput.vue";
+import EmailInput from "./inputs/EmailInput.vue";
+export default {
+  name: "BaseForm",
+  components: { ValidationObserver, PasswordInput, EmailInput },
+  props: {
+    headline: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      currentType: "password",
+    };
+  },
+
+  methods: {
+    onSubmit() {
+      this.$emit("formAction", this.email, this.password);
+    },
+  },
+};
+</script>
+
+<style lang="sass" module>
+.form
+  display: flex
+  flex-direction: column
+  align-items: center
+  width: 100%
+  background-color: $main-bg
+  &_headline
+    font-weight: 800
+    color: $headline-color
+    font-size: 1.25em
+  &_submit
+    padding: 1em 3em
+    border: 1px solid transparent
+    border-radius: 8px
+    box-shadow: 0px -5px 5px -5px rgba(34, 60, 80, 0.6)
+    cursor: pointer
+    background-color: $elements-bg
+    color: $text-main
+    text-transform: uppercase
+    letter-spacing: 1px
+    &:hover
+      transform: scale(0.99)
+</style>
