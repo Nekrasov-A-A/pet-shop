e69dae1d92a8fc39f59a5543b77a952ccda6002c
nekrasov-a-a <a.alex.nekrasov@gmail.com>
2022-08-26 22:07:23 +0300

    feat(vuex): add username (set,get) to user.js module

--- src/store/user.js
+++ src/store/user.js
@@ -1,16 +1,22 @@
 export default {
   state: {
     loggedIn: "",
+    userName: "",
   },
   getters: {
     isLoggedIn: (s) => s.loggedIn,
+    getUserName: (s) => s.userName,
   },
   mutations: {
     setLoggedIn: (s, value) => (s.loggedIn = value),
+    setUserName: (s, value) => (s.userName = value),
   },
   actions: {
     changeLoggedIn({ commit }, value) {
       commit("setLoggedIn", value);
+    },
+    changeUserName({ commit }, payload) {
+      commit("setUserName", payload);
     },
   },
 };
