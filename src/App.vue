e2d6a812d72fa322c1f7d2156610e9cb04bbb452
nekrasov-a-a <a.alex.nekrasov@gmail.com>
2022-08-26 22:00:58 +0300

    feat(vuex): add username to vuex
    set username from displayname(firebase)

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,75 @@
 <template>
   <div id="app">
-    <router-view />
+    <component :is="currentLayout">
+      <transition mode="out-in" @enter="fadeIn" @leave="fadeOut">
+        <router-view />
+      </transition>
+    </component>
   </div>
 </template>
 
 <script>
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
+import DefaultLayout from "./views/layouts/DefaultLayout.vue";
+import EmptyLayout from "./views/layouts/EmptyLayout.vue";
+import gsap from "gsap";
+
 export default {
+  components: { DefaultLayout, EmptyLayout },
+
   data() {
-    return {};
+    return {
+      currentLayout: "DefaultLayout",
+    };
   },
+
+  computed: {
+    ...mapGetters(["isLoggedIn"]),
+    someshit() {
+      return getAuth().currentUser;
+    },
+  },
+
+  watch: {
+    $route: {
+      immediate: true,
+      handler(route) {
+        this.currentLayout = route.meta.layout || "DefaultLayout";
+      },
+    },
+  },
+
   created() {
     this.controlAuth();
   },
+
   methods: {
-    ...mapActions(["changeLoggedIn"]),
+    ...mapActions(["changeLoggedIn", "changeUserName"]),
+
     controlAuth() {
       const auth = getAuth();
-      this.changeLoggedIn(auth);
+      this.changeLoggedIn(auth?.currentUser);
+      if (this.isLoggedIn) {
+        this.changeUserName(auth.currentUser.displayName);
+      }
       onAuthStateChanged(auth, () => {
         this.changeLoggedIn(!!auth.currentUser);
+        if (this.isLoggedIn) {
+          if (auth.currentUser.displayName) {
+            this.changeUserName(auth.currentUser.displayName);
+          }
+        } else {
+          this.changeUserName("");
+        }
       });
+    },
+
+    fadeOut(el) {
+      gsap.fromTo(el, { y: 0 }, { y: "100%" });
+    },
+    fadeIn(el) {
+      gsap.fromTo(el, { y: "100%" }, { y: 0 });
     },
   },
 };
