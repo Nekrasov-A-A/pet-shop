ab5670f4ca7253b0afaa3bd4efa4665f9ff9273c
nekrasov-a-a <a.alex.nekrasov@gmail.com>
2022-08-26 22:03:25 +0300

    feat(firebase): add methods: setusername and changepassword

--- src/firebase/firebase-auth.js
+++ src/firebase/firebase-auth.js
@@ -3,6 +3,8 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
+  updateProfile,
+  updatePassword,
 } from "firebase/auth";
 
 let singleton;
@@ -13,27 +15,43 @@ class FirebaseAuth {
     return singleton;
   }
 
-  #auth = getAuth();
-
   async loginUser(email, password) {
     try {
-      await signInWithEmailAndPassword(this.auth, email, password);
+      const auth = getAuth();
+      await signInWithEmailAndPassword(auth, email, password);
     } catch (error) {
       throw new Error("Такого пользователя нет");
     }
   }
 
-  async createUser(email, password) {
+  async createUser(email, password, name = "userName") {
     try {
-      await createUserWithEmailAndPassword(this.auth, email, password);
+      const auth = getAuth();
+      await createUserWithEmailAndPassword(auth, email, password);
+      await this.setUserName(name);
     } catch (error) {
       throw new Error("Такой пользователь уже создан");
     }
   }
 
+  async setUserName(name) {
+    try {
+      const auth = getAuth();
+      await updateProfile(auth.currentUser, { displayName: name });
+    } catch (error) {
+      throw new Error("Что-то пошло не так");
+    }
+  }
+
+  async changeUserPassword(newPassword) {
+    const auth = getAuth();
+    await updatePassword(auth.currentUser, newPassword);
+  }
+
   async signOutUser() {
     try {
-      await signOut(this.auth);
+      const auth = getAuth();
+      await signOut(auth);
     } catch (error) {
       throw new Error("Что-то пошло не так");
     }
