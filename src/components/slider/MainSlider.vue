dbd12bfa16cbfcd07145252d80814da8f33b3300
nekrasov-a-a <a.alex.nekrasov@gmail.com>
2022-09-17 22:09:52 +0300

    feat(components): add MainSlider.vue
     all photo in  directory assets/images/slider require in MainSlider.vue component and render as SwiperSlide.
    >> Photo name should be orderNumber-slide-specialName. And it equal to component name in components/slider/slides.
    >> For example: 1-slide-women.jpg required SlideWomen component.

---
+++ src/components/slider/MainSlider.vue
@@ -0,0 +1,150 @@
+<template>
+  <Swiper
+    :class="$style.slider"
+    class="swiper"
+    :options="swiperOptions"
+    ref="mainSlider"
+  >
+    <SwiperSlide
+      :class="$style.slide"
+      v-for="(item, index) in imagesData"
+      :key="item + index"
+    >
+      <div
+        :class="$style.slideInner"
+        :style="{ backgroundImage: `url(${item.imagePath})` }"
+      >
+        <Component :is="item.componentName" />
+      </div>
+    </SwiperSlide>
+    <div
+      class="swiper-pagination"
+      slot="pagination"
+      :class="$style.pagination"
+    ></div>
+  </Swiper>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import SlideMen from "./slides/SlideMen.vue";
+import SlideCollection from "./slides/SlideCollection.vue";
+import SlideWomen from "./slides/SlideWomen.vue";
+import "swiper/css/swiper.css";
+export default {
+  name: "MainSlider",
+  components: {
+    Swiper,
+    SwiperSlide,
+    SlideMen,
+    SlideCollection,
+    SlideWomen,
+  },
+
+  data() {
+    return {
+      imagesData: [],
+      swiperOptions: {
+        direction: "vertical",
+        initialSlide: 2,
+        spaceBetween: 0,
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          renderBullet(_, className) {
+            return `<div class="${className} swiper-pagination-bullet-custom" ></div>`;
+          },
+        },
+        mousewheel: { enabled: true },
+        keyboard: { enabled: true },
+      },
+    };
+  },
+  mounted() {
+    let images = require.context("../../assets/images/slider", true, /\.jpg$/);
+    this.importAll(images);
+  },
+  methods: {
+    importAll(images) {
+      images.keys().forEach((key) => {
+        let componentName = this.getComponentName(key);
+        let imageData = {
+          imagePath: images(key),
+          componentName,
+        };
+
+        this.imagesData.push(imageData);
+      });
+    },
+    getComponentName(key) {
+      let value = key.split("-");
+      let name =
+        this.firstLetterToUpperCase(value[1]) +
+        this.firstLetterToUpperCase(this.removeImageType(value[2]));
+      return name;
+    },
+    firstLetterToUpperCase(string) {
+      return string.slice(0, 1).toUpperCase() + string.slice(1);
+    },
+    removeImageType(string) {
+      return string.replace(/[.jpg]/gi, "");
+    },
+  },
+};
+</script>
+
+<style lang="sass" module>
+.image
+  width: 100%
+  height: 100%
+.slider
+  width: 100vw
+  height: 100vh
+  overflow: hidden
+.slide
+  width: 100vw
+  height: 100vh
+.slideInner
+  width: 100%
+  height: 100%
+  display: flex
+  align-items: center
+  justify-content: center
+.pagination
+  display: flex
+  flex-direction: column
+  position: absolute
+  top: 50%
+  left: 95%
+  transform: translateY(-50%)
+  background-color: transparent
+  @media screen and (max-width: $sm)
+    left: 90%
+</style>
+
+<style lang="sass">
+.swiper-pagination-bullet-custom
+  display: flex
+  justify-content: center
+  align-items: center
+  font-size: 0
+  background-color: $slider-dots-bg-color
+  cursor: pointer
+  border-radius: 50%
+  margin: 8px 0
+  width: 20px
+  height: 20px
+  opacity: 0.75
+  transition: all .3s
+  &:hover
+    opacity: 1
+.swiper-pagination-bullet-active
+  height: 45px
+  border-radius: 12px
+  background-color: $slider-dots-bg-color
+  opacity: 1
+  &:hover
+    background-color: $slider-dots-bg-color
+</style>
