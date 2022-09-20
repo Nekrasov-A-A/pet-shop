13fd8cd53bd2420395cee9e7b9116e3f1ffe4ad3
nekrasov-a-a <a.alex.nekrasov@gmail.com>
2022-09-20 23:50:04 +0300

    feat(view): add home page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="$style.homeWrapper">
+    <MainSlider />
+  </div>
+</template>
+<script>
+import MainSlider from "../components/slider/MainSlider.vue";
+
+export default {
+  name: "HomeView",
+  components: { MainSlider },
+};
+</script>
+<style lang="sass" module>
+.homeWrapper
+  background-color: $main-bg
+  overflow: hidden
+</style>
