9056a5de8632b4f90d561d50f200ff222b70b20d
nekrasov-a-a <a.alex.nekrasov@gmail.com>
2022-09-20 23:52:25 +0300

    feat(component): add loader without styles

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,26 @@
+<template>
+  <section :class="$style.loader">Loading...</section>
+</template>
+
+<script>
+export default {
+  name: "AppLoader",
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+};
+</script>
+
+<style lang="sass" module>
+.loader
+  position: absolute
+  width: 100vw
+  height: 100vh
+  display: flex
+  justify-content: center
+  align-items: center
+  z-index: 599
+  background-color: #fff
+</style>
