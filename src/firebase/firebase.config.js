2ca0ec4c1736bf17e58240d9f63e27fb2eb23d41
nekrasov-a-a <a.alex.nekrasov@gmail.com>
2022-09-21 01:03:37 +0300

    feat(firebase): add firestore and init

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-initializeApp({
+const firebase = initializeApp({
   apiKey: "AIzaSyCpdacJNwh7qxUKeKdp5DT3vTSDwoijvv4",
   authDomain: "pet-shop-b3e22.firebaseapp.com",
   projectId: "pet-shop-b3e22",
@@ -8,3 +9,7 @@ initializeApp({
   messagingSenderId: "744286127439",
   appId: "1:744286127439:web:4ab4971d7351439591713a",
 });
+
+const db = getFirestore(firebase);
+
+export default db;
