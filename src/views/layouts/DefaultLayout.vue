e0cb962c61d6cb220e5e01d18ac93d8100aa0a80
nekrasov-a-a <a.alex.nekrasov@gmail.com>
2022-09-20 23:51:08 +0300

    feat(layouts): add empty and default layouts

---
+++ src/views/layouts/DefaultLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <TheHeader />
+    <main :class="$style.wrapper">
+      <slot></slot>
+    </main>
+  </section>
+</template>
+
+<script>
+import TheHeader from "../../components/header/TheHeader.vue";
+export default {
+  name: "DefaultLayout",
+  components: { TheHeader },
+};
+</script>
+
+<style lang="sass" module>
+.wrapper
+  background-color: $main-bg
+</style>
